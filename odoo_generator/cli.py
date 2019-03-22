43667996c8f8277b3468b43bc6b885e0f15412fe
Cinxgler Mariaca Minda <cinxgler.mariaca@idu.gov.co>
2019-03-22 12:25:09 -0500

    Added skeleton of plugin management

--- odoo_generator/cli.py
+++ odoo_generator/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Console script for odoo_generator."""
+from yapsy.PluginManager import PluginManager
 import sys
 import click
 
@@ -11,6 +12,23 @@ def main(args=None):
     click.echo("Replace this message by putting your code into "
                "odoo_generator.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
+
+    manager = PluginManager()
+    manager.setPluginPlaces(["plugins"])
+    manager.collectPlugins()
+
+
+    builder = odoo_generator.MetamodelBuilder()
+    add_attribute_functions = []
+    for pluginInfo in simplePluginManager.getPluginsOfCategory("MetadataBuilder"):
+        manager.activatePluginByName(pluginInfo.name)
+        add_attribute_functions.append(pluginInfo.plugin_object.add_attributes)
+
+    module = builder.build(line_generator, add_attribute_functions)
+
+    for pluginInfo in simplePluginManager.getPluginsOfCategory("CodeGenerator"):
+        pluginInfo.plugin_object.generate_code(module)
+
     return 0
 
 
